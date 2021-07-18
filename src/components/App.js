d784d24de198909ef071aa500311fa12539eb0a7
Norbert <kurzawski.n@gmail.com>
2021-07-18 18:22:11 +0200

    cleansed typical react app code and added basic css

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import "../styles/App.css";
+
+export const App = () => {
+    return (
+        <div className="App">
+            Hello World <br/>
+            I am an empty template of Application <br/>
+            Happy hacking
+        </div>
+    )
+}
